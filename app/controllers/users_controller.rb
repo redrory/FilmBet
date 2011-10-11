3fcb8e20d2a9f38ab238b6efc6d662164719fe53
Rory Walker <redrory@gmail.com>
2011-10-11 16:27:11 -0500

    first push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   	@secondMovie  = FeedEntry.find(2).title
   	@thirdMovie  = FeedEntry.find(3).title
   	@fourthMovie  = FeedEntry.find(4).title
+  	@movie_list = FeedEntry.all
+
   end
 
 end
