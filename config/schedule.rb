02c56b3e13836c60f1ef0f941d149c1a108d0cdd
Rory Walker <redrory@gmail.com>
2011-10-26 07:25:26 -0500

    Cron working for must create, delete db next

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every 2.minutes do
-	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')"
+	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')" , :environment => :development
 end
