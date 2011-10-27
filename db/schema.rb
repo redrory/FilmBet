3ec672ce35fa27624d3c6b7f239a3ad5b6d4b642
Rory Walker <redrory@gmail.com>
2011-10-27 17:40:06 -0500

    Schema issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111023012419) do
-
-  create_table "feed_entries", :force => true do |t|
-    t.string   "title"
-    t.string   "guid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 20111027114547) do
 
   create_table "movies", :force => true do |t|
     t.integer  "user_id"
