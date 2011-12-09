5dbea612883145417f707253c9534db70b4f11c7
Rory Walker <redrory@gmail.com>
2011-12-09 16:40:26 -0500

    FB Two Months later

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,11 @@
 #every :monday, :at => '3pm' do
-every 2.minutes do	
+#every 2.minutes do	
+every :monday, :at => '3pm' do
 	runner "FeedEntry.deleteTable", :environment => :development
 	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')" , :environment => :development
 end
 
-every 3.days do
+every :monday, :at => '5pm' do
 	runner "NewFeedEntry.deleteTable", :environment => :development
 	runner "NewFeedEntry.update_from_feed('http://www.fandango.com/rss/newmovies.rss')" , :environment => :development
 end
