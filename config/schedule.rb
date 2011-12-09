ec8860bf1618e0d8e862cab3fc3ab7a3ec6b21eb
Rory Walker <redrory@gmail.com>
2011-12-09 17:31:32 -0500

    Cron Job, production added to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,12 +3,12 @@
 #every :monday, :at => '3pm' do
 
 every 2.minutes do	
-	runner "FeedEntry.deleteTable", :environment => :development, :production
-	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')" , :environment => :development,:production
+	runner "FeedEntry.deleteTable", :environment => :production
+	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')" , :environment => :production
 end
 
 #every :monday, :at => '5pm' do
 every 2.minutes do	
-	runner "NewFeedEntry.deleteTable", :environment => :development,:production
-	runner "NewFeedEntry.update_from_feed('http://www.fandango.com/rss/newmovies.rss')" , :environment => :development, :production
+	runner "NewFeedEntry.deleteTable", :environment => :production
+	runner "NewFeedEntry.update_from_feed('http://www.fandango.com/rss/newmovies.rss')" , :environment => :production
 end
