143d7a95a50bc82aca905e9746b5f9ecd65f0bdd
Rory Walker <redrory@gmail.com>
2011-12-28 15:41:03 -0500

    Add envir production to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,13 +2,13 @@
 #every 2.minutes do	
 #every :monday, :at => '3pm' do
 
-every 2.minutes do	
+every 3.minutes do	
 	runner "FeedEntry.deleteTable",  :environment => :development,  :environment => :production
 	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')" ,  :environment => :development, :environment => :production
 end
 
 #every :monday, :at => '5pm' do
-every 2.minutes do	
+every 3.minutes do	
 	runner "NewFeedEntry.deleteTable",  :environment => :development,  :environment => :production
 	runner "NewFeedEntry.update_from_feed('http://www.fandango.com/rss/newmovies.rss')" ,  :environment => :development,  :environment => :production
 end
