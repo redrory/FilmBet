577ec8391883ac10a21228fa1136f390a1026ebc
Rory Walker <redrory@gmail.com>
2011-12-28 15:50:29 -0500

    Whenever/Local, scheduler for live

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,13 +2,13 @@
 #every 2.minutes do	
 #every :monday, :at => '3pm' do
 
-every 3.minutes do	
-	runner "FeedEntry.deleteTable",  :environment => :development,  :environment => :production
-	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')" ,  :environment => :development, :environment => :production
+every :monday, :at => '3pm' do
+	runner "FeedEntry.deleteTable",  :environment => :development
+	runner "FeedEntry.update_from_feed('http://www.fandango.com/rss/top10boxoffice.rss')" ,  :environment => :development
 end
 
-#every :monday, :at => '5pm' do
-every 3.minutes do	
-	runner "NewFeedEntry.deleteTable",  :environment => :development,  :environment => :production
-	runner "NewFeedEntry.update_from_feed('http://www.fandango.com/rss/newmovies.rss')" ,  :environment => :development,  :environment => :production
+
+every :monday, :at => '5pm' do
+	runner "NewFeedEntry.deleteTable",  :environment => :development
+	runner "NewFeedEntry.update_from_feed('http://www.fandango.com/rss/newmovies.rss')" ,  :environment => :development
 end
