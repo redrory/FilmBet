42791cc9737b1a6dd78329b0e23506642cd8c190
Rory Walker <redrory@gmail.com>
2012-02-14 07:38:00 -0500

    Update Box Office RSS

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -1,11 +1,12 @@
 class FeedEntry < ActiveRecord::Base
-  feed_url ="http://www.fandango.com/rss/top10boxoffice.rss"
+  #feed_url ="http://www.fandango.com/rss/top10boxoffice.rss"
+  feed_url = "http://movies.msn.com/rss/topboxoffice"
 	 def self.update_from_feed(feed_url)
     feed = Feedzirra::Feed.fetch_and_parse(feed_url)
     feed.entries.each do |entry|
       #unless exists? :guid => entry.id
         create(
-          :title         => entry.title,
+          :title         => entry.title[0..-11],
           :guid         => entry.id
         )
       end
